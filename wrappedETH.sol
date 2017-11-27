d29415f03bf245646bee6b53e7e0f4da35739bed
themandalore <fett.nicholas@yahoo.com>
2017-11-27 15:51:48 -0500

    Update wrappedETH.sol

--- wrappedETH.sol
+++ wrappedETH.sol
@@ -1,78 +1,98 @@
-pragma solidity ^0.4.14;
+pragma solidity ^0.4.17;
 
-import 'github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol';
+contract Wrapped_Ether {
 
-contract Wrapped_Ether{
-  string public name;
-  uint256 public totalSupply;
-  uint256 public value;
-  uint256 public tokenValue;
+  using SafeMath for uint256;
+
+  /*Variables*/
+
+  //ERC20 fields
+  string public name = "Wrapped Ether";
+  uint public total_supply;
+
+
+  //ERC20 fields
+  mapping(address => uint) balances;
+  mapping(address => mapping (address => uint)) allowed;
+
+  /*Events*/
+
   event Transfer(address indexed _from, address indexed _to, uint _value);
   event Approval(address indexed _owner, address indexed _spender, uint _value);
   event StateChanged(bool _success, string _message);
-  mapping(address => uint256) balances;
-  mapping(address => mapping (address => uint256)) allowed;
 
- function Wrapped_Ether(uint _tokenValue, uint _multiplier, string _name){
-    tokenValue = _tokenValue * _multiplier; //token value is in Wei.  So 1000000000000000000 * 1 is an Ether
-    name = _name;
- }
- function CreateToken() payable{
-   require(msg.value > 0);
-   balances[msg.sender] = balances[msg.sender] + SafeMath.div(msg.value,tokenValue);
-   totalSupply = totalSupply + SafeMath.div(msg.value,tokenValue);
-   value = tokenValue * totalSupply;
-  }
-  
-  function withdraw(uint256 _amount) {
-    transfer(address(this),_amount);
-    msg.sender.transfer(tokenValue * _amount);
-    totalSupply = totalSupply -  _amount;
-    value = tokenValue * totalSupply;
+  /*Functions*/
+
+  //This function creates tokens equal in value to the amount sent to the contract
+  function CreateToken() public payable {
+    require(msg.value > 0);
+    balances[msg.sender] = balances[msg.sender].add(msg.value);
+    total_supply = total_supply.add(msg.value);
   }
 
-  function balanceOf(address _owner) constant returns (uint256 balance) {return balances[_owner];}
-
-  function transfer(address _to, uint256 _amount)returns (bool success){
-      if (balances[msg.sender] >= _amount 
-        && _amount > 0
-        && balances[_to] + _amount > balances[_to]) {
-        balances[msg.sender] -= _amount;
-        balances[_to] += _amount;
-        Transfer(msg.sender, _to, _amount);
-        return true;
-      } else {
-        return false;
-      }
+  /*
+  * This function 'unwraps' an _amount of Ether in the sender's balance by transferring Ether to them
+  *
+  * @param "_amount": The amount of the token to unwrap
+  */
+    function withdraw(uint _value) public{
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        total_supply = total_supply.sub(_value);
+        msg.sender.transfer(_value);
     }
 
-  function transferFrom(
-    address _from,
-    address _to,
-    uint256 _amount
-    ) returns (bool success){
+  //Returns the balance associated with the passed in _owner
+  function balanceOf(address _owner) public constant returns (uint bal) { return balances[_owner]; }
+
+  /*
+  * Allows for a transfer of tokens to _to
+  *
+  * @param "_to": The address to send tokens to
+  * @param "_amount": The amount of tokens to send
+  */
+  function transfer(address _to, uint _amount) public returns (bool success) {
+    if (balances[msg.sender] >= _amount
+    && _amount > 0
+    && balances[_to] + _amount > balances[_to]) {
+      balances[msg.sender] = balances[msg.sender].sub(_amount);
+      balances[_to] = balances[_to].add(_amount);
+      Transfer(msg.sender, _to, _amount);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /*
+  * Allows an address with sufficient spending allowance to send tokens on the behalf of _from
+  *
+  * @param "_from": The address to send tokens from
+  * @param "_to": The address to send tokens to
+  * @param "_amount": The amount of tokens to send
+  */
+  function transferFrom(address _from, address _to, uint _amount) public returns (bool success) {
     if (balances[_from] >= _amount
     && allowed[_from][msg.sender] >= _amount
     && _amount > 0
     && balances[_to] + _amount > balances[_to]) {
-    balances[_from] -= _amount;
-    allowed[_from][msg.sender] -= _amount;
-    balances[_to] += _amount;
-    Transfer(_from, _to, _amount);
+      balances[_from] = balances[_from].sub(_amount);
+      allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_amount);
+      balances[_to] = balances[_to].add(_amount);
+      Transfer(_from, _to, _amount);
       return true;
     } else {
-     return false;
+      return false;
     }
   }
-  
-  function approve(address _spender, uint256 _amount) returns (bool success) {
-      allowed[msg.sender][_spender] = _amount;
-      Approval(msg.sender, _spender, _amount);
-      return true;
-    }
 
-  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-      }
+  //Approves a _spender an _amount of tokens to use
+  function approve(address _spender, uint _amount) public returns (bool success) {
+    allowed[msg.sender][_spender] = _amount;
+    Approval(msg.sender, _spender, _amount);
+    return true;
+  }
+
+  //Returns the remaining allowance of tokens granted to the _spender from the _owner
+  function allowance(address _owner, address _spender) constant public returns (uint remaining) { return allowed[_owner][_spender]; }
 
 }
